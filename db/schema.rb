1fdc7cfcad504982e5bf22342f5fd31f3283455c
xxcheesexx <kmkr0629@gmail.com>
2017-11-25 11:14:37 +0900

    Finish user registration

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20171117083153) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "name"
   end
 
